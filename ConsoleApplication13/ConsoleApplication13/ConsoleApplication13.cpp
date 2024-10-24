7dca9ad05ee5ac2bed05ce8739b00c3c65e3d5af
gastonientyu <killinnool@gmail.com>
2024-10-24 21:14:23 +0500

    Add files via upload
    Первый комит

---
+++ ConsoleApplication13/ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+class circle {
+private:
+    double radius;
+public:
+    circle(double rad) {
+        radius = rad;
+    }
+    double getLength() {
+        return 2 * 3.14 * radius;
+    }
+    double getArea() {
+        return 3.14 * radius * radius;
+    }
+};
+int main()
+{
+    setlocale(0, "");
+    double radius;
+    cout << "Введите радиус: ";
+    cin >> radius;
+    circle mcircle(radius);
+    cout << "\nДлина окружности: " << mcircle.getLength();
+    cout << "\nПлощадь окружности: " << mcircle.getArea();
+    return 0;
+}
+
